e1cbe6d5ffe7ed2cf5b960c25b3fc28b9981307d
jungheeeun <jungheeeun272@gmail.com>
2023-07-13 11:45:45 +0900

    타스 수동으로 트랜스파일하기

---
+++ example.js
@@ -0,0 +1,5 @@
+function greet(name) {
+    console.log("Hello, ".concat(name));
+}
+var person = "Jung heeeun";
+greet(person);
---
+++ example.ts
@@ -0,0 +1,6 @@
+function greet(name: string){
+  console.log(`Hello, ${name}`);
+}
+
+const person = "Jung heeeun";
+greet(person);
